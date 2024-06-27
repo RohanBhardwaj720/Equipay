0e52432f4a106e4a0b9707d38ea3d071a8a37fb2
Rohan Bhardwaj <rohanbhardwaj720@gmail.com>
2024-06-28 03:33:38 +0530

    updated header css and jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
-import styles from '../styles/header.module.css'
+import React from 'react';
+import styles from '../styles/header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import '../styles/confirmAlert.css'; 
 
 function Header(props) {
+  const handleAuthAction = () => {
+    if (props.isAuth) {
+      confirmAlert({
+        title: 'Confirm Log Out',
+        message: 'Are you sure you want to log out?',
+        buttons: [
+          {
+            label: 'Yes',
+            onClick: () => {
+              props.setAuth(false);
+              console.log('Logged out successfully');
+            }
+          },
+          {
+            label: 'No',
+            onClick: () => console.log('Log out cancelled')
+          }
+        ]
+      });
+    } else {
+      props.onSignIn();
+    }
+  };
+
   return (
-    <header>
+    <header className={styles.header}>
       <h1>
-        <i id={styles.equi}>Equi</i>
+        <i className={styles.equi}>Equi</i>
         <i>Pay</i>
       </h1>
-      {props.isAuth ? (
-        <button
-          id={styles.logout}
-          onClick={() => {
-            props.setAuth(false)
-            console.log('Logout successful:')
-          }}
-        >
-          Logout
-        </button>
-      ) : null}
+      <button
+        className={styles.authButton}
+        onClick={handleAuthAction}
+      >
+        <FontAwesomeIcon icon={props.isAuth ? faSignOutAlt : faSignInAlt} />
+        {' '}
+        {props.isAuth ? 'Log Out' : 'Sign In'}
+      </button>
     </header>
-  )
+  );
 }
-export default Header
+
+export default Header;
